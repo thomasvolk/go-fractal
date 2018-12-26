a5a84d011d256eb609cf96b978e352e0f5995eaf
Thomas Volk (Inspiron-7437) <thomas@my-volk.de>
2018-12-26 23:41:37 +0100

    add tests

---
+++ fractal_test.go
@@ -0,0 +1,48 @@
+package fractal
+
+import "testing"
+
+func TestXStepYStep(t *testing.T) {
+	c := ComplexSet{
+		Real:      Range{-1.5, 1.5},
+		Imaginary: Range{-1.0, 1.0},
+		Algorithm: Mandelbrot,
+	}
+	p := NewPlane(c, 300, 200, 10)
+	expected := 0.01
+	result := p.XStep()
+	if expected != result {
+		t.Errorf("Xstep %v != %v", expected, result)
+	}
+	expected = 0.01
+	result = p.YStep()
+	if expected != result {
+		t.Errorf("Ystep %v != %v", expected, result)
+	}
+}
+
+func TestCrop(t *testing.T) {
+	c := ComplexSet{
+		Real:      Range{-1.5, 1.5},
+		Imaginary: Range{-1.0, 1.0},
+		Algorithm: Mandelbrot,
+	}
+	p := NewPlane(c, 300, 200, 10)
+	pc := p.Crop(Box{0, 0, 150, 100})
+
+	expected := 0.005
+	result := pc.XStep()
+	if expected != result {
+		t.Errorf("Xstep %v != %v", expected, result)
+	}
+	expected = 0.005
+	result = pc.YStep()
+	if expected != result {
+		t.Errorf("Ystep %v != %v", expected, result)
+	}
+	cc := pc.ComplexSet()
+	if cc.Real.Start != -1.5 || cc.Real.End != 0.0 || cc.Imaginary.Start != -1.0 || cc.Imaginary.End != 0.0 {
+		t.Errorf("ComplexSet")
+	}
+
+}
