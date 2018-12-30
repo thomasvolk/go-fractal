f7080fef0b3310d8598fc703799096038bebb948
Thomas Volk (Inspiron-7437) <thomas@my-volk.de>
2018-12-30 19:17:27 +0100

    split zoom frames from selection

--- zoom.go
+++ zoom.go
@@ -8,17 +8,22 @@ func (p Plane) CircleAutoZoom(x int, y int, radiusDivisor float64, angleStep flo
 	box := Box{0, 0, p.width, p.height}.InnerBox(x, y)
 	rx := float64(box.Width) / radiusDivisor
 	ry := float64(box.Height) / radiusDivisor
-	currentDeviation := 0.0
-	bestFrame := p
+	var frames []Plane
 	for theta := 0.0; theta < 360.0; theta += angleStep {
 		px := x + int(rx*math.Cos(theta))
 		py := y + int(ry*math.Sin(theta))
-		frame := p.Crop(box.InnerBox(px, py))
+		frames = append(frames, p.Crop(box.InnerBox(px, py)))
+	}
+
+	currentDeviation := 0.0
+	bestFrame := p
+	for _, frame := range frames {
 		d := frame.Deviation()
 		if d > currentDeviation {
 			currentDeviation = d
 			bestFrame = frame
 		}
+
 	}
 	return bestFrame
 }
@@ -26,23 +31,21 @@ func (p Plane) CircleAutoZoom(x int, y int, radiusDivisor float64, angleStep flo
 func (p Plane) RasterAutoZoom(division int) Plane {
 	wd := p.width / division
 	hd := p.height / division
-
 	boxes := division * division
-	frames := make([]Box, boxes)
+	frames := make([]Plane, boxes)
 	for f := 0; f < boxes; f++ {
 		x := (f * wd) % p.width
 		y := (f * hd) % p.height
-		frames[f] = Box{x, y, wd, hd}
+		frames[f] = p.Crop(Box{x, y, wd, hd})
 	}
 
 	currentDeviation := 0.0
 	bestFrame := p
 	for _, f := range frames {
-		frame := p.Crop(f)
-		d := frame.Deviation()
+		d := f.Deviation()
 		if d > currentDeviation {
 			currentDeviation = d
-			bestFrame = frame
+			bestFrame = f
 		}
 	}
 	return bestFrame
