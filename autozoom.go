22a53c28990f2694358a47f2fbd34fdfe62f1203
Thomas Volk (Inspiron-7437) <thomas@my-volk.de>
2018-12-30 14:56:19 +0100

    zoom methods

--- autozoom.go
+++ autozoom.go
@@ -8,6 +8,10 @@ import (
 )
 
 func (p Plane) CircleAutoZoom(x int, y int, radiusDivisor float64, angleStep float64) Plane {
+	return p.CircleZoom(utils.Deviation, x, y, radiusDivisor, angleStep)
+}
+
+func (p Plane) CircleZoom(selector func(part [][]int) float64, x int, y int, radiusDivisor float64, angleStep float64) Plane {
 	box := model.Box{0, 0, p.width, p.height}.InnerBox(x, y)
 	rx := float64(box.Width) / radiusDivisor
 	ry := float64(box.Height) / radiusDivisor
@@ -18,7 +22,7 @@ func (p Plane) CircleAutoZoom(x int, y int, radiusDivisor float64, angleStep flo
 		py := y + int(ry*math.Sin(theta))
 		frame := box.InnerBox(px, py)
 		part := utils.Crop(p.values, frame)
-		d := utils.Deviation(part)
+		d := selector(part)
 		if d > currentDeviation {
 			currentDeviation = d
 			bestFrame = frame
@@ -26,8 +30,11 @@ func (p Plane) CircleAutoZoom(x int, y int, radiusDivisor float64, angleStep flo
 	}
 	return p.Crop(bestFrame)
 }
-
 func (p Plane) RasterAutoZoom(division int) Plane {
+	return p.RasterZoom(utils.Deviation, division)
+}
+
+func (p Plane) RasterZoom(selector func(part [][]int) float64, division int) Plane {
 	wd := p.width / division
 	hd := p.height / division
 
